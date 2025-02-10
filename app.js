9d8f9c05787c926424f6ff8c88013d4e8640f46a
reinaldocantidio <reinaldocantidio@gmail.com>
2025-02-10 20:47:33 -0300

    agregar_amigos

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
 
 let amigos = [];
 
+function adicionarAmigo() {
+    let inputAmigo = document.getElementById("amigo");
+    let nome = inputAmigo.value.trim();
+    
+    if (nome === "") {
+        alert("Por favor, insira um nome.");
+        return;
+    }
+
+    amigos.push(nome);
+
+    atualizarListaAmigos();
+
+    inputAmigo.value = "";
+}
+
+
